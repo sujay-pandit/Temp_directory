965cbaeb6aeb0c519419865bf02a77c071658509
Sujay Pandit <pandit8@msee337lnx11.ecn.purdue.edu>
2023-01-30 15:55:52 -0500

    repo cleanup

--- timingpower.tcl
+++ timingpower.tcl
@@ -1,5 +1,5 @@
 log_begin timingpower.log
-read_liberty sky130_fd_sc_hvl__tt_100C_3v30.lib
+read_liberty ./lib/sky130_fd_sc_hvl__tt_100C_3v30.lib
 read_verilog synth_core.yv
 link_design mkccore_axi4
 create_clock -name clk -period 4000 {CLK}
